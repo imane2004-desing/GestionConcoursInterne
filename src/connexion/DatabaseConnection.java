3c09cc0197ba8599e20eb00ad213efab5fd82d63
imane elkourari <imanekourari@gmail.com>
2025-03-12 19:11:02 +0100

    Update DatabaseConnection.java

--- src/connexion/DatabaseConnection.java
+++ src/connexion/DatabaseConnection.java
@@ -11,8 +11,7 @@ package connexion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/gestion_concours";
     private static final String USER = "root"; 

4d65ff156505a03e91366da96e0b56f47dbf603f
imane elkourari <imanekourari@gmail.com>
2025-03-14 16:53:23 +0100

    Update ResultatConcours.java

--- src/beans/ResultatConcours.java
+++ src/beans/ResultatConcours.java
@@ -9,14 +9,10 @@ package beans;
  * @author hp
  */
 public class ResultatConcours {
-    
     private ConcoursInterne concoursId;
     private Participant participantId;
     private float note;
-
-  
     public ResultatConcours( ConcoursInterne concoursId, Participant participantId, float note) {
-     
         this.concoursId = concoursId;
         this.participantId = participantId;
         this.note = note;
@@ -26,16 +22,12 @@ public ResultatConcours( ConcoursInterne concoursId, Participant participantId, 
         this.participantId = participantId;
         this.note = note;
     }
-   
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
     public  ConcoursInterne getConcoursId() { return concoursId; }
     public void setConcoursId( ConcoursInterne concoursId) { this.concoursId = concoursId; }
-
     public  Participant getParticipantId() { return participantId; }
     public void setParticipantId( Participant participantId) { this.participantId = participantId; }
-
     public float getNote() { return note; }
     public void setNote(float note) { this.note = note; }
 }

e7e802777184117a1e40cd3d253f4a5c34404990
imane elkourari <imanekourari@gmail.com>
2025-03-14 16:52:43 +0100

    Update Participant.java

--- src/beans/Participant.java
+++ src/beans/Participant.java
@@ -13,7 +13,6 @@ public class Participant {
     private String nom;
     private String prenom;
     private String email;
-
     // Constructeur
     public Participant(int id, String nom, String prenom, String email) {
         this.id = id;
@@ -21,17 +20,13 @@ public class Participant {
         this.prenom = prenom;
         this.email = email;
     }
-
     // Getters et Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
-
     public String getPrenom() { return prenom; }
     public void setPrenom(String prenom) { this.prenom = prenom; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 }
